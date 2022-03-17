e3168fc6bd9fa89143e024a3e822ccf0bdbd0c43
Mason Wilkins <mwilkins17@gmail.com>
2022-03-17 12:54:11 -0400

    added a commented-out function to determine if a file has a day number, added notes to function

--- produce_summary.py
+++ produce_summary.py
@@ -39,11 +39,32 @@
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
 
+from dataclasses import dataclass
+from webbrowser import get
+import os
+
+
+
+# def get_day_count():
+#     for file in os.listdir():
+#         day_count = []
+#         if ".txt" in file:
+#             file_tokens = file.split("-")
+#             elements_of_file_tokens = file_tokens[3].split(".")
+            
+#             day_count += elements_of_file_tokens[0]
+#             print("day count is:", day_count)
+#     return day_count
+# get_day_count()
+# print(os.listdir())
+
+
+
 day_count = 1
-def daily_report(day_count):
-    
+def print_daily_report():
+    """determines"""
     while day_count < 4:
-        the_file = open (f"um-deliveries-day-{day_count}.txt")
+        the_file = open (f"um-deliveries-day-{day_count}.txt")  #opens the file according to the day count, starting at 1
         for line in the_file:
             line = line.rstrip()
             # print(line)
@@ -57,4 +78,4 @@ def daily_report(day_count):
             print(f"Day {day_count}: Delivered {count} {melon}s for a total of ${amount}")
         day_count += 1
         the_file.close()
-daily_report(day_count)
+print_daily_report()
